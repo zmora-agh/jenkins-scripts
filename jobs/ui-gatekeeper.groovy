f9a4ec033b1224eb5957cb8a928f072952bf86c1
Mateusz Nowotyński <maxmati@memleak.pl>
2017-10-17 14:33:07 +0200

    Fix nodejs

--- jobs/ui-gatekeeper.groovy
+++ jobs/ui-gatekeeper.groovy
@@ -1,11 +1,26 @@
-node {
-   stage('Preparation') { 
-      git 'https://github.com/zmora-agh/zmora-ui.git'
-   }
-   stage('Build') {
-      tools {nodejs "default"}
-	  steps {
-	    sh 'npm --version'
-	  }
-   }
+pipeline {
+		agent any
+		tools {nodejs "default"}
+		stages {
+				stage('Preparation') {
+						steps {
+								git 'https://github.com/zmora-agh/zmora-ui.git'
+						}
+				}
+				stage('Install') {
+						steps {
+								sh 'npm install'
+						}
+				}
+				stage('Build') {
+						steps {
+								sh 'npm run build'
+						}
+				}
+				stage('Lint') {
+						steps {
+								sh 'npm run lint'
+						}
+				}
+		}
 }
